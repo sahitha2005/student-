c5018733b9403e54a006b71f95894e048ebbb778
SAHITHA S <sahitha.mca2024@adhiyamaan.in>
2025-05-10 20:56:15 +0530

    Add files via upload
    I have created html file along with css .Server.js and User.js file to save the data in the mongodb.

--- server.js
+++ server.js
@@ -2,53 +2,53 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const User = require('./models/User');  
+const User = require('./models/User'); 
 
 const app = express();
 const PORT = 3000;
 
-mongoose.connect('mongodb://localhost:27017/innovationDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((err) => {
-    console.error("MongoDB connection error:", err);
-});
+mongoose.connect('mongodb://localhost:27017/innovationDB')
+  .then(() => {
+      console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+      console.error("MongoDB connection error:", err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'loginpage.html'));
+    res.sendFile(path.join(__dirname, 'loginpage.html')); 
 });
 
 app.post('/login', async (req, res) => {
-    
+    console.log(req.body); 
     const { name, regno, dob, department, tenth_percentage, twelth_percentage, ug_percentage, hobbies } = req.body;
 
+    
+    const hobbiesArray = hobbies ? (Array.isArray(hobbies) ? hobbies : [hobbies]) : [];
+
     try {
-        
         const user = new User({
             name,
             regno,
-            dob,
+            dob: new Date(dob),  
             department,
             tenth_percentage,
             twelth_percentage,
             ug_percentage,
-            hobbies: hobbies || []  
+            hobbies: hobbiesArray
         });
 
-        
         await user.save();
-        res.send(' Login details saved to MongoDB!');
+        res.send('Login details saved to MongoDB!');
     } catch (err) {
         console.error("Error saving user:", err);
-        res.status(500).send(' Error saving to database');
+        res.status(500).send('Error saving to database');
     }
 });
 
 app.listen(PORT, () => {
-    console.log(` Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
